e10f00650d0073d8d712ef8709014fc8a84795c3
Cherry <manyberry38@gamil.com>
2023-12-04 23:20:11 +0200

    test

--- quiz.js
+++ quiz.js
@@ -4,13 +4,16 @@ const currentQuestionShow = document.querySelector(".quiz__question");
 const result = document.querySelector(".quiz__result");
 const resultScore = document.querySelector(".quiz__result-score");
 const content = document.querySelector(".quiz__content");
-const restartBtn = document.querySelector(".quiz__restart");
+// const restartBtn = document.querySelector(".quiz__restart");
 const answerBtns = document.querySelectorAll(".quiz__button");
 // пары слов
-const storagePairs = localStorage.getItem("words");
-let pairs = storagePairs === null ? [] : JSON.parse(storagePairs);
+// const storagePairs = localStorage.getItem("words");
+// let pairs = storagePairs === null ? [] : JSON.parse(storagePairs);
 let quantity = 12;
-export const exsiciseQuiz = (pairs) => {
+export const exsiciseQuiz = (pairs, lang) => {
+    const sourceLang = lang === "en" ? "enWord" : "ukWord";
+    const targetLang = lang === "en" ? "ukWord" : "enWord";
+    // console.log(sourceLang, targetLang);
     const selected = pairs.sort(() => 0.5 - Math.random()).slice(0, quantity); // тут взяли 10 слов из общего количества в словаре
 
     let currentQuestion = 0; // номер текущего вопроса, начиная с 0
@@ -20,30 +23,31 @@ export const exsiciseQuiz = (pairs) => {
     result.style.display = "none";
 
     const showWords = (i) => {
-        //отрисовка номера вопроса и пары слов, перевод зависет от рандома или монетки
-
         const answers = pairs
             .filter(({ id }) => id !== selected[i]["id"])
             .sort(() => 0.5 - Math.random())
             .slice(0, 3)
-            .map(({ ukWord }) => ukWord);
-        answers.push(selected[i]["ukWord"]);
+            .map((pair) => {
+                return pair[targetLang];
+            });
+        answers.push(selected[i][targetLang]);
         answers.sort(() => 0.5 - Math.random());
         answerBtns.forEach((btn, idx) => {
             btn.textContent = answers[idx];
             btn.dataset.value = answers[idx];
         });
+        console.log(answers);
+        const en = selected[i][sourceLang];
 
-        const en = selected[i]["enWord"];
         item.textContent = en;
 
         currentQuestionShow.textContent = `current question: ${
             i + 1
-        } / ${quantity}`; //номер вопроса начиная с 1
+        } / ${quantity}`;
     };
 
-    const handleAnswer = (answer, i) => {
-        if (answer === selected[i]["ukWord"]) {
+    const handleAnswer = (answer, i, lang) => {
+        if (answer === selected[i][lang]) {
             score += 1;
         }
     };
@@ -57,7 +61,7 @@ export const exsiciseQuiz = (pairs) => {
     answerBtns.forEach((btn) => {
         btn.onclick = (e) => {
             const value = e.target.dataset.value;
-            handleAnswer(value, currentQuestion);
+            handleAnswer(value, currentQuestion, targetLang);
             currentQuestion++;
             if (currentQuestion < quantity) {
                 showWords(currentQuestion);
@@ -70,7 +74,7 @@ export const exsiciseQuiz = (pairs) => {
     showWords(currentQuestion); //отрисовка первого вопроса при загрузке нашей игры
 };
 
-restartBtn.onclick = () => {
-    exsiciseQuiz(pairs);
-};
+// restartBtn.onclick = () => {
+//     exsiciseQuiz(pairs, lang);
+// };
 // 10 вынети в конст и сделать функционал для изменени кол-ва слов
