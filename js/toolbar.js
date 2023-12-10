05d6166cdbd6345fc78c5cfa94d8e8b5d3ce2207
Vishenka93 <104006603+Vishenka93@users.noreply.github.com>
2023-12-10 07:52:10 +0000

    refactor

---
+++ js/toolbar.js
@@ -0,0 +1,61 @@
+import { exirciseTrueOrFalse } from "./exercises/trueOfFalse.js";
+import { exsiciseQuiz } from "./exercises/quiz.js";
+import { myWords } from "./vocabulary/myWords.js";
+
+const toolbarBtnWords = document.querySelector(".toolbar__words");
+const toolbarBtnTrueOrFalse = document.querySelector(
+    ".exercises__true-of-false"
+);
+const toolbarBtnQuizEn = document.querySelector(".exercises__quiz-en");
+const toolbarBtnQuizUk = document.querySelector(".exercises__quiz-uk");
+const contentWords = document.querySelector(".wrapper-content");
+const contentTrueOrFalse = document.querySelector(".true-or-false-content");
+const contentQuiz = document.querySelector(".quiz");
+
+const storagePairs = localStorage.getItem("words");
+let pairs = storagePairs === null ? [] : JSON.parse(storagePairs);
+
+
+const toolbarBtns = [
+    toolbarBtnWords,
+    toolbarBtnTrueOrFalse,
+    toolbarBtnQuizEn,
+    toolbarBtnQuizUk,
+];
+const toolbarContents = [contentWords, contentTrueOrFalse, contentQuiz];
+
+
+
+const handleClickTab = (tabName, lang) => {
+    toolbarContents.forEach((content) => {
+        content.style.display =
+            content.dataset.name === tabName ? "block" : "none";
+    });
+    toolbarBtns.forEach((button) => {
+        if (button.dataset.name === tabName && button.dataset.lang === lang) {
+            button.classList.add("selected-tab");
+        } else {
+            button.classList.remove("selected-tab");
+        }
+    });
+};
+toolbarBtns.forEach((btn) => {
+    btn.onclick = (e) => {
+        const tabName = e.target.dataset.name;
+        const lang = e.target.dataset.lang;
+        handleClickTab(tabName, lang);
+        if (tabName === "true-or-false") {
+            exirciseTrueOrFalse(pairs);
+        } else if (tabName === "quiz" && lang === "en") {
+            exsiciseQuiz(pairs, "en");
+        } else if (tabName === "quiz" && lang === "uk") {
+            exsiciseQuiz(pairs, "uk");
+        } else if(tabName === "words") {
+            myWords(pairs)
+        }
+    };
+});
+
+
+toolbarBtnWords.click()
+
