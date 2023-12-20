ec98e63e4462e8bd556b9745438af8c11dca72bf
Cherry <manyberry38@gamil.com>
2023-12-20 15:18:08 +0200

    create checkbox

--- js/stats/history.js
+++ js/stats/history.js
@@ -1,8 +1,5 @@
 const table = document.querySelector(".history__table-body");
-const checkbox = document.querySelector(".checkbox")
-
-
-
+const checkbox = document.querySelector(".checkbox");
 
 const getDateStr = (time) => {
     const t = new Date(time);
@@ -16,24 +13,30 @@ const getDateStr = (time) => {
 export const showUserHistory = () => {
     table.innerHTML = "";
     const history = JSON.parse(localStorage.getItem("history")) || [];
-    
-    
+
     history.forEach(({ timeStamp, name, stats }) => {
         const tr = document.createElement("tr");
-        const pes = stats.split("/")
-        const persant = pes[0] / pes[1] * 100
-        // console.log(stats.split("/").join(""))
+        const pes = stats.split("/");
+        const persant = (pes[0] / pes[1]) * 100;
         const dateStr = getDateStr(timeStamp);
         [dateStr, name, stats].forEach((el) => {
             const td = document.createElement("td");
             td.textContent = el;
-            if(persant <= 40) {
-                td.classList.add("td-class", "red")
-            } else if (persant > 40 && persant <= 80){
-                td.classList.add("td-class", "yellow")
-            } else {
-                td.classList.add("td-class", "green") 
-            }
+            td.classList.add("td-class");
+            checkbox.addEventListener("change", function () {
+                if (checkbox.checked) {
+                    if (persant <= 40) {
+                        td.classList.add("red");
+                    } else if (persant > 40 && persant <= 80) {
+                        td.classList.add("yellow");
+                    } else {
+                        td.classList.add("green");
+                    }
+                } else {
+                    td.classList.remove("red", "green", "yellow");
+                }
+            });
+
             tr.appendChild(td);
         });
 
